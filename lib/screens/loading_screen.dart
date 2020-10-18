9fd63aa7cef7142678c926a33f021336e19682a4
“[Adekomi <adekomiabdulbaqi3@gmail.com>
2020-10-18 17:06:50 +0100

    http status code check

--- lib/screens/loading_screen.dart
+++ lib/screens/loading_screen.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:clima/services/location.dart';
+import 'package:http/http.dart' as http;
 
 class LoadingScreen extends StatefulWidget {
   @override
@@ -7,16 +9,33 @@ class LoadingScreen extends StatefulWidget {
 
 class _LoadingScreenState extends State<LoadingScreen> {
   @override
+  void initState() {
+    super.initState();
+    getLocation();
+    print('This line is triggered');
+  }
+
+  void getLocation() async {
+    Location location = Location();
+
+    await location.getLocation();
+    print(location.longitude);
+    print(location.latitude);
+  }
+
+  void getData() async {
+    http.Response response =
+        await http.get('https://samples.openweathermap.org/data/2'
+            '.5/weather?lat=35&lon=139&appid=1a5bcd7476da8a7f120b3f718d659be5');
+    if (response.statusCode == 200) {
+      print(response.body);
+    } else {
+      print(response.statusCode);
+    }
+  }
+
   Widget build(BuildContext context) {
-    return Scaffold(
-      body: Center(
-        child: RaisedButton(
-          onPressed: () {
-            //Get the current location
-          },
-          child: Text('Get Location'),
-        ),
-      ),
-    );
+    getData();
+    return Scaffold();
   }
 }
